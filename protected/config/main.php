de5ef0ed4e6ede21f2d7bd5a0269b6f4710e96cd
Joseph Lessard <joseph.lessard@yahoo.com>
2011-07-13 12:24:46 -0400

    fixed init installation

--- protected/config/main.php
+++ protected/config/main.php
@@ -51,15 +51,15 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		
+		/*
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=zeunic',
+			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 		),
-		
+		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
