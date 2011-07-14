9f6fb94a848e7fe3781296c5affc6cc3052e6517
Joseph Lessard <joseph.lessard@yahoo.com>
2011-07-14 15:09:19 -0400

    navigation sprite and logo

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" lang="en" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/reset.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
@@ -12,14 +11,33 @@
 	<header>
 		<q><em>we <strong>guarantee</strong></em><br /><em>you will <strong>appreciate</strong></em><br /><small>the <strong>effort</strong> we put into </small><strong>our work.</strong></q>
 	</header>
-
-	<?php echo $content; ?>
+	
+	<div id="container">
+	
+		<a id="logo" href="<?php echo Yii::app()->request->baseUrl; ?>/index.php">
+			<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.png" alt="Zeunic" /> 
+		</a>
+		
+		<nav>
+			<ul>
+				<li id="nav_about"><a href="#">About Us</a></li>
+				<li id="nav_search"><a href="#">Search</a></li>
+				<li id="nav_contact"><a href="#">Contact</a></li>
+				<li id="nav_login"><a href="#">Login</a></li>
+				<li id="nav_work" class="active"><a href="#">Our Work</a></li>
+			</ul>
+			<p>Our Work</p>
+		</nav>
+	
+		<?php echo $content; ?>
+	
+	</div>
 	
 	<footer>
 		<aside>
 			<img src="images/icons/@zeunic.png" alt="@Zeunic" /><br />
 			<q></q>
-			<cite>7 minutes ago via Twitter for Android</cite>
+			<cite></cite>
 		</aside>
 	</footer>
 	
