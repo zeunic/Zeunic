b4fb469a274cca90147c6298de2dddc107ef207f
Stephen Rivas JR <me@stephenrivasjr.com>
2011-07-15 21:03:29 -0400

    About Us copy, CSS adjustments

--- protected/views/site/about.php
+++ protected/views/site/about.php
@@ -1,2 +1,22 @@
 <?php $this->beginWidget('application.components.NavWidget', array('page'=>1)); ?>
 <?php $this->endWidget(); ?>
+
+<div id="main">
+
+<article>
+	<section class="about-us">
+	<h1>&#8220;We're about the details. <em>Your details.</em>&#8221;</h1>
+	
+	<p>Zeunic was started in Spring 2011 around an idea that passion, focus, and energy could create great things if you could see both the big picture and the details that nobody else pays attention to.</p>
+
+	<p>Have you ever noticed how much nicer a glass of tea looks served with a mint leaf? Or that feeling of relief when you thought you lost everything only to find out the web application was saving for you? What about when you fell completely in love with a particular brand, not because of what it does or how it does it, but because of how it made you feel inside?</p>
+
+	<p><strong>Yea, us too. Those are game changers.</strong></p>
+
+	<p>Zeunic is about creativity and vision meeting the effective simplicity of focusing on the details. The greatest web application in the world won't work without the little things that make it a great experience, nor would the best little detail features matter without a vision to make them part of a greater whole.</p>
+
+	<p>That's what we are about. Why don't you <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/contact">tell us about you?</a></p>
+	</section>
+</article>
+
+</div>
