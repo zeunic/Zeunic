178694b74311c6305332efca0502be29cda399f6
Stephen Rivas JR <me@stephenrivasjr.com>
2011-07-17 04:48:17 -0400

    Testing OurWork jQ animations

--- protected/views/site/portfolio.php
+++ protected/views/site/portfolio.php
@@ -1,2 +1,21 @@
 <?php $this->beginWidget('application.components.NavWidget', array('page'=>0)); ?>
 <?php $this->endWidget(); ?>
+
+<div id="main">
+
+<article>
+	<section class="about-us">
+	
+	<p>web | mobile | brand | media</p>
+	
+	<div class="left-thumb">
+		<img class="lg-thumb" src="<?php echo Yii::app()->request->baseUrl; ?>/images/thumbs/590x120_vignette.jpg" />
+		<img class="sm-thumb" src="<?php echo Yii::app()->request->baseUrl; ?>/images/thumbs/190x120_vignette.jpg" />
+	</div>
+	<div class="center-thumb"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/thumbs/190x120_vignette.jpg" /></div>
+	<div class="right-thumb"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/thumbs/190x120_vignette.jpg" /></div>
+	
+	</section>
+</article>
+
+</div>
