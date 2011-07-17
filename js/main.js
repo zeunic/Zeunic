c7f7fbb3cce356165185ed514ed82af54b242b92
Joseph Lessard <joseph.lessard@yahoo.com>
2011-07-17 02:34:21 -0400

    forgot to save

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(function(){
 		var name = $(this).val();
 		var sig = $('#signature');
 		if(name == ''){
-			sig.html('Thanks').animate({height:30},300);
+			sig.html('Thanks.').animate({height:30},300);
 		} else {
 			sig.animate({height:70}, 300).html('Thanks,<br />'+name);
 		}
