de76e7bd51071ddd6d4981ed1b3d6026c7ad5f31
Stephen Rivas JR <me@stephenrivasjr.com>
2011-07-21 19:40:54 -0400

    Login Form Style & Functionality ready for review

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -1,10 +1,14 @@
 <?php $this->beginWidget('application.components.NavWidget', array('page'=>4)); ?>
 <?php $this->endWidget(); ?>
 
-<div id="main">
-	
+<div id="admin">
+<!-- page specific CSS sheets loaded here until a compiler/compressor could be implemented -->
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/admin.css" />
+
+	<p>To access your account controls and information, please enter the credentials we provided you below:</p>
+
 	<?php $form=$this->beginWidget('CActiveForm', array(
-    'id'=>'login',
+    'id'=>'login_form',
     'enableAjaxValidation'=>true,
     'enableClientValidation'=>true
 )); ?>
@@ -16,17 +20,21 @@
  
     <div class="row">
         <?php echo $form->label($model,'username'); ?>
-        <?php echo $form->textField($model,'username') ?>
+        <?php echo $form->textField($model,'username', array('placeholder'=>'user name')) ?>
     </div>
  
     <div class="row">
         <?php echo $form->label($model,'password'); ?>
-        <?php echo $form->passwordField($model,'password') ?>
+        <?php echo $form->passwordField($model,'password', array('placeholder'=>'password')) ?>
     </div>
  
     <div class="row submit">
-        <?php echo CHtml::submitButton('Login'); ?>
+    	<a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/admin/index">Login</a>
+    	<?php echo CHtml::submitButton('Login'); ?>
     </div>
 
 	<?php $this->endWidget(); ?>
 </div>
+
+<!-- page specific JS loads -->
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/login.js"></script>
