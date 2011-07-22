5c840dfa38afb91a2a36561e7e91329786bde27d
Stephen Rivas JR <me@stephenrivasjr.com>
2011-07-22 03:58:45 -0400

    Another try

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -8,7 +8,7 @@ if(!$ajax){
 <div id="main">
 <? endif; ?>
 
-<h2>Testing Deployment Copy. PLEEEEEAAAASSSSSSSEEEEEEE.</h2>
+<h2>Testing Deployment Copy. PLEEEEEAAAASSSSEE. Work.</h2>
 
 <? if(!$ajax): ?>
 </div>
