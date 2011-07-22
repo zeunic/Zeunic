b7874a9f5caeaa77c5c60527bb69b2380b0ca1f8
Stephen Rivas JR <me@stephenrivasjr.com>
2011-07-22 04:51:14 -0400

    Another try 7...getting close!

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -8,7 +8,7 @@ if(!$ajax){
 <div id="main">
 <? endif; ?>
 
-<h2>Testing Deployment Copy. PLEEEEEAAAASSSSEE. Work...God this might be so money. I think I'm on the right track any how.</h2>
+<h2>Testing Deployment Copy. PLEEEEEAAAASSSSEE. Work...God this might be so money. I think I'm on the right track any how. I think I'm getting close!</h2>
 
 <? if(!$ajax): ?>
 </div>
