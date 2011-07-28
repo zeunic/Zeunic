9a60ca0d168ecca059340f6393ae6506b8a8f11c
Stephen Rivas JR <stephen@zeunic.com>
2011-07-28 05:37:36 -0400

    Bad commit log entry

--- protected/views/site/about.php
+++ protected/views/site/about.php
@@ -22,11 +22,10 @@ if(!$ajax){
 
 	<p>Have you ever noticed how much nicer a glass of tea looks served with a mint leaf? Or that feeling of relief when you thought you lost everything only to find out the web application was saving your work for you? What about when you fell completely in love with a particular brand, not because of what it does or how it does it, but because of how it made you feel?</p>
 
-	<p><strong>Yeah, us too. Those are game changers.</strong></p>
+	<!--<p><strong>We create projects that revolve around the flourish of the details.</strong></p> -->
 
-	<!-- <p>Zeunic is about creativity and vision meeting the effective simplicity of focusing on the details. The greatest web application in the world won't work without the little things that make it a great experience, nor would the best little detail features matter without a vision to make them part of a greater whole.</p> -->
-	
-	<p>Zeunic is about creativity and vision meeting the effective simplicity of the smallest details.  The greatest web application in the world will not work without the little things that make make it a great experience, nor would the smallest details matter without a vision to make them a part of the greater whole.</p>
+	<p><strong>Zeunic is about creativity and vision meeting the effective simplicity of the smallest details.</strong></p>
+	<p>The greatest web application in the world will not work without the little things that make make it a great experience, nor would the smallest details matter without a vision to make them a part of the greater whole.</p>
 
 	<p>That's what we are about. Why don't you <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/contact">tell us about you?</a></p>
 	</section>
@@ -35,17 +34,19 @@ if(!$ajax){
 		<h2>Meet the Team</h2>
 		
 		<div class="minibio">
+			<h3>Stephen Rivas JR <span>| Project Manager</span></h3>
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/about/stephen.png" alt="stephen" />
 			<ul>
 				<li class="tweet"><a href="twitter.com/olivierlacan">@olivierlacan</a> lol, I need to retain reference to a setInterval and setTimeout in a .bind()/.live() scenario. I'll call some time.</li>
-				<li>Weight: <strong>180lbs</strong></li>
-				<li>Favorite Food: <strong>Mexican</strong></li>
-				<li>Likes: <strong>InDesign</strong></li>
+				<li>Likes: <strong>Javascript && CouchDB;</strong></li>
+				<li>Super Power: <strong>Teleporting.</strong></li>
+				<li>Proud Father: <strong>Yup!</strong></li>
 			</ul>
-			<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue.</p>
+			<p>I love to push myself, often times past what I am capable of. If I'm not struggling, or being challenged I'm not satisfied. I love the industry, and dreaming big. Also currently the FS SGA Campus Senator for WDD, and Project Manager for Zeunic.</p>
 		</div>
 		
 		<div class="minibio">
+			<h3>Joe Lessard <span>| Lead Developer</span></h3>
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/about/joe.png" alt="joe" />
 			<ul>
 				<li class="tweet">Hangin with Edward Maya tonight <a href="http://t.co/Yb5OnO4">http://t.co/Yb5OnO4</a></li>
@@ -57,6 +58,7 @@ if(!$ajax){
 		</div>
 		
 		<div class="minibio">
+			<h3>Parker W Young <span>| Creative Director</span></h3>
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/about/parker.png" alt="parker" />
 			<ul>
 				<li class="tweet">Serif and Sans-serif fonts play nice together, you just gotta lay down some ground rules first. <a href="fb.me/12jIDAl0K" data-external="true">fb.me/12jIDAl0K</a></li>
@@ -64,10 +66,11 @@ if(!$ajax){
 				<li>Dislikes: Then ending to Inception</li>
 				<li>Favorite Site: Adobe.com</li>
 			</ul>
-			<p>Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+			<p>The name would be Parker. The game is design. The tools of my trade are my eyes. People's creativity comes from their eye; a creative eye supplements the creative mind. In my world, it's all about grabbing people by the collar, pulling them in and immersing them in the meaningful aesthetics of my work.</p>
 		</div>
 		
 		<div class="minibio">
+			<h3>Spencer Bailey <span>| Front-End Developer</span></h3>
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/about/spencer.png" alt="spencer" />
 			<ul>
 				<li class="tweet">Finished up the new Zeunic teaser site with help of my esteemed colleagues! Check it out over at zeunic.com</li>
