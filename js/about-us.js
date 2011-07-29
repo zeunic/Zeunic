6135a91e8f20b97f5387061e90f55606e440e82c
Stephen Rivas JR <stephen@zeunic.com>
2011-07-29 11:40:03 -0400

    About-Us permissions fixed

--- js/about-us.js
+++ js/about-us.js
@@ -3,6 +3,7 @@ $(function(){
 	var twProfiles = ["sprjrx", "JoeLessard", "parkrr", "sbaileydev"];
 
 	$('.tweet').each(function(index){
+		// update for git
 		$(this).tweets({
 			tweets: 1,
 			username: twProfiles[index],
