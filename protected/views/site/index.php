a656a5323866acb1e6703d3e4b8e470803c48593
Joseph Lessard <joseph.lessard@yahoo.com>
2011-07-30 06:03:54 -0400

    beginning of home page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -11,39 +11,25 @@ if(!$ajax){
 <div id="main">
 <? endif; ?>
 <div id="home">
-<h1>Recharge your brand with Zeunic!</h1>
-
-<div id="newsgallery">
-	<ul id="slider">
-		<li>Slide 1</li>
-		
-		<li>Slide 2</li>
-		
-		<li>Slide 3</li>
-		<li><img width="590" height="180"</li>
-	</ul>
-</div>
 
 <article>
 	<section>
 		<h2>Mobile</h2>
-		<p>Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-		<p><a href="">Mobile Projects</a></p>
+		<p><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/project/8"><span id="innershadow"></span><img alt="Voice Tricks" src="<?php echo Yii::app()->request->baseUrl; ?>/images/home_voice.png" /><span class="projectText">Voice Tricks</span></a></p>
 	</section>
 	
 	<section>
 		<h2>Web</h2>
-		<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-		<p><a href="">Web Projects</a></p>
+		<p><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/project/12"><span id="innershadow"></span><img alt="JillZarin.com" src="<?php echo Yii::app()->request->baseUrl; ?>/images/home_jill.jpg" /><span class="projectText">JillZarin.com</span></a></p>
 	</section>
 	
 	<section>
 		<h2>Brand</h2>
-		<p>Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec id elit non mi porta gravida at eget metus.</p>
-		<p><a href="">Brand Projects</a></p>
+		<p><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/project/4"><span id="innershadow"></span><img alt="NESOI" src="<?php echo Yii::app()->request->baseUrl; ?>/images/home_nesoi.jpg" /><span class="projectText">NESOI</span></a></p>
 	</section>
 </article>
 
+<h1>Recharge your brand with Zeunic!</h1>
 </div>
 <? if(!$ajax): ?>
 </div>
