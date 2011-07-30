76c2a7b1c018589eba863c010403eea94fd779cb
Joseph Lessard <joseph.lessard@yahoo.com>
2011-07-30 08:44:00 -0400

    fixing html5 validation errors

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,28 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta http-equiv="Content-Language" content="EN" />
 	<meta name="description" content="Mobile Development, Web Design & Branding" />
 	<meta name="keywords" content="zeunic, zeunic.com, mobile, mobile development, web, web design, web site, branding, corporate branding, orlando, florida," />
 	<meta name="author" content="Stephen Rivas JR, Joe Lessard & Parker W. Young" />
 	<meta name="robots" content="all" />
-	<meta name="copyright" content="Copyright © 2011 Zeunic. All Rights Reserved." />
-	<meta name="abstract" content="Mobile Development, Web Design & Branding" />
+	<meta name="rights" content="Copyright © 2011 Zeunic. All Rights Reserved." />
+	<meta name="dcterms.description" content="Mobile Development, Web Design & Branding" />
 
 	<!-- Social Open Graph --> 
-	<meta property="og:title" content="zeunic | home" />
-	<meta property="og:type" content="company" />
-	<meta property="og:image" content="http://www.zeunic.com/images/og.jpg" />
-	<meta property="og:url" content="http://www.zeunic.com/" />
-	<meta property="og:description" content="Mobile Development, Web Design & Branding" />
-	<meta property="og:site_name" content="zeunic.com" />
-	<meta property="fb:admins" content="555537034, 19213532, 1469677507"/>
-	<meta property="og:email" content="social@zeunic.com" />
-	<meta property="og:locality" content="Orlando" />
-	<meta property="og:region" content="FL" />
-	<meta property="og:postal-code" content="32779" /> 
-	<meta property="og:country-name" content="USA" />
+	<meta name="og:title" content="zeunic | home" />
+	<meta name="og:type" content="company" />
+	<meta name="og:image" content="http://www.zeunic.com/images/og.jpg" />
+	<meta name="og:url" content="http://www.zeunic.com/" />
+	<meta name="og:description" content="Mobile Development, Web Design & Branding" />
+	<meta name="og:site_name" content="zeunic.com" />
+	<meta name="fb:admins" content="555537034, 19213532, 1469677507"/>
+	<meta name="og:email" content="social@zeunic.com" />
+	<meta name="og:locality" content="Orlando" />
+	<meta name="og:region" content="FL" />
+	<meta name="og:postal-code" content="32779" /> 
+	<meta name="og:country-name" content="USA" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/reset.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/admin.css" />
