2f2d5f23f8065452fd3374711bbd134de29ef16a
Joseph Lessard <joseph.lessard@yahoo.com>
2011-07-31 06:01:20 -0400

    fixing portfolio height bug

--- js/main.js
+++ js/main.js
@@ -155,7 +155,7 @@ $(function(){
 						container.css({height:'auto'});
 						$('#content #nav').stickyfloat({ duration: 900, tartOffset: 200, offsetY: 0 });
 					});
-				}, 500);
+				}, 1000);
 				
 				
 				$('.loader').remove();
